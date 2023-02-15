5a0af791bbd42c7838496c2756386ebcfa0b70f0
Djan <124775935+DjanRamadan@users.noreply.github.com>
2023-02-15 12:36:19 +0200

    Add files via upload

---
+++ Week.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week
+{
+    internal class Week
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a day (Please pick a number 1-7!):");
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            int day = int.Parse(Console.ReadLine());
+
+            if (day >= 1 && day <= 7)
+            {
+                Console.WriteLine(days[day - 1]);
+            }
+            else
+                Console.WriteLine("Invalid Day");
+        }
+    }
+}
